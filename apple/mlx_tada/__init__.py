08165e80de3264d7e77d3a90dc3dc2952f4c229a
Taiga Ishida <tonythetaiga@gmail.com>
2026-03-20 11:32:42 -0400

    Simplify __init__.py imports, add __all__

--- apple/mlx_tada/__init__.py
+++ apple/mlx_tada/__init__.py
@@ -1,6 +1,12 @@
-from .audio import save_wav as save_wav
-from .config import GenerationOutput as GenerationOutput
-from .config import InferenceOptions as InferenceOptions
-from .config import Reference as Reference
-from .model import TadaForCausalLM as TadaForCausalLM
-from .model import setup_logging as setup_logging
+from .audio import save_wav
+from .config import GenerationOutput, InferenceOptions, Reference
+from .model import TadaForCausalLM, setup_logging
+
+__all__ = [
+    "GenerationOutput",
+    "InferenceOptions",
+    "Reference",
+    "TadaForCausalLM",
+    "save_wav",
+    "setup_logging",
+]
